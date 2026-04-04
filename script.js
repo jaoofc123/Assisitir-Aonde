de2a9ac3c78b01c8ec6861d8e932d25dc96b123c
jaoofc123 <105529532+jaoofc123@users.noreply.github.com>
2026-04-04 09:36:32 -0300

    Implement game filtering based on competition selection
    Add event listener to filter games based on selection.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.getElementById("competicao").addEventListener("change", function() {
 
+let valor = this.value
+let jogos = document.querySelectorAll(".jogo")
+
+jogos.forEach(jogo => {
+
+if(valor === "todos"){
+jogo.style.display = "block"
+}
+else if(jogo.classList.contains(valor)){
+jogo.style.display = "block"
+}
+else{
+jogo.style.display = "none"
+}
+
+})
+
+})
