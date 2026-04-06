69faa4d609c9eb3f0b29c1c18336f9bdf28153d4
jaoofc123 <105529532+jaoofc123@users.noreply.github.com>
2026-04-06 14:49:48 -0300

    Refactor event listener setup and date updates

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.getElementById("competicao").addEventListener("change", function() {
+document.addEventListener("DOMContentLoaded", function(){
+
+// FILTRO DE COMPETIÇÃO
+let filtro = document.getElementById("competicao")
+
+if(filtro){
+filtro.addEventListener("change", function(){
 
 let valor = this.value
 let jogos = document.querySelectorAll(".jogo")
@@ -16,5 +22,44 @@ jogo.style.display = "none"
 }
 
 })
+
+})
+}
+
+// ATUALIZAR DATAS DOS JOGOS
+let hoje = new Date()
+
+let ano = hoje.getFullYear()
+let mes = String(hoje.getMonth()+1).padStart(2,'0')
+let dia = String(hoje.getDate()).padStart(2,'0')
+
+let hojeFormatado = ano+"-"+mes+"-"+dia
+
+let datas = document.querySelectorAll(".data-jogo")
+
+datas.forEach(data => {
+
+let dataJogo = data.getAttribute("data-date")
+
+if(dataJogo === hojeFormatado){
+data.innerText = "Hoje"
+}
+else{
+
+let partes = dataJogo.split("-")
+data.innerText = partes[2]+"/"+partes[1]+"/"+partes[0]
+
+}
+
+})
+
+// ATUALIZAR TITULO "JOGOS DE HOJE"
+let titulo = document.getElementById("titulo-hoje")
+
+if(titulo){
+
+titulo.innerText = "Jogos de Hoje ("+dia+"/"+mes+"/"+ano+")"
+
+}
 
 })
